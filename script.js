e5bc0db221408bdd2c44bfab508e7fdec6e685c5
Dennis <deweis@gmx.ch>
2018-06-14 10:17:03 +0200

    Add name and age to the results page

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 /*jshint esversion: 6 */
 function showInput() {
+  /* Change header section */
   document.getElementById('title').innerText = 'Your Answers';
   document.getElementById('description').innerText = 'Thank you for submitting!';
+
+  /* Remove the form */
   document.getElementById('survey-form').style.display = 'none';
+
+  /* Show the results */
+  const theName = document.getElementById('name').value;
+  const theAge = document.getElementById('number').value;
+
   document.getElementById('results').style.display = 'block';
-  document.getElementById('resGender').innerText = 'your Gender'; //document.getElementById("user_input").value;
+  document.getElementById('resGender').innerText = 'your Gender';
+  document.getElementById('resName').innerText = theName;
+  document.getElementById('resAge').innerText = theAge;
 }
