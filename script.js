9322ebe8256c68857711872e43ae93e6f0ee08f9
Dennis <deweis@gmx.ch>
2018-06-15 11:32:48 +0200

    Fix required not working
    Change from onClick to eventlistener on submit

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 /*jshint esversion: 6 */
-function showInput() {
-  /* Change header section */
-  document.getElementById('title').innerText = 'Your Answers';
-  document.getElementById('description').innerText = 'Thank you for submitting!';
+document.getElementById('survey-form').addEventListener('submit', function (event) {
+  event.preventDefault();   // Thank you https://stackoverflow.com/questions/21338476/addeventlistener-on-form-submit
 
   /* Remove the form */
   document.getElementById('survey-form').style.display = 'none';
 
   /* Show the results */
+  document.getElementById('results').style.display = 'block';
+  document.getElementById('title').innerText = 'Your Answers';
+  document.getElementById('description').innerText = 'Thank you for submitting!';
+
+  /* Get the answers */
   const theGender = document.querySelector('input[name=inlineRadioOptions]:checked').value;
   const theName = document.getElementById('name').value;
   const theAge = document.getElementById('number').value;
   const theEmail = document.getElementById('email').value;
 
-  document.getElementById('results').style.display = 'block';
   document.getElementById('resGender').innerText = theGender;
   document.getElementById('resName').innerText = theName;
   document.getElementById('resAge').innerText = theAge;
   document.getElementById('resEmail').innerText = theEmail;
-}
+
+});
