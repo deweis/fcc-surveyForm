fe37fc1ea2033f58a686e8f06be596636caf5894
Dennis <deweis@gmx.ch>
2018-06-16 09:12:23 +0200

    Add preference to results page

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 /*jshint esversion: 6 */
 document.getElementById('survey-form').addEventListener('submit', function (event) {
   event.preventDefault();   // Thank you https://stackoverflow.com/questions/21338476/addeventlistener-on-form-submit
+  showResults();
+});
 
+function showResults() {
   /* Remove the form */
   document.getElementById('survey-form').style.display = 'none';
 
@@ -10,15 +13,19 @@ document.getElementById('survey-form').addEventListener('submit', function (even
   document.getElementById('title').innerText = 'Your Answers';
   document.getElementById('description').innerText = 'Thank you for submitting!';
 
-  /* Get the answers */
+  getAnswers();
+}
+
+function getAnswers() {
   const theGender = document.querySelector('input[name=inlineRadioOptions]:checked').value;
   const theName = document.getElementById('name').value;
   const theAge = document.getElementById('number').value;
   const theEmail = document.getElementById('email').value;
+  const thePreference = document.getElementById('dropdown').value;
 
   document.getElementById('resGender').innerText = theGender;
   document.getElementById('resName').innerText = theName;
   document.getElementById('resAge').innerText = theAge;
   document.getElementById('resEmail').innerText = theEmail;
-
-});
+  document.getElementById('resPreference').innerText = thePreference;
+}
